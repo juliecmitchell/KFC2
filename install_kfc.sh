df33410a2d2b8c87482cfe78067d8069b3e96471
juliecmitchell <julie.mitchell@gmail.com>
2024-11-25 06:57:57 -0600

    updates

--- install_kfc.sh
+++ install_kfc.sh
@@ -2,18 +2,31 @@
 
 cd src
 
-rm ../bin/KFC2_exec/core_rim_1
-rm ../bin/KFC2_exec/core_rim_2
-rm ../bin/KFC2_exec/mut_compensation_1
-rm ../bin/KFC2_exec/splitpdb
+rm -f ../bin/KFC2_exec/core_rim_1
+rm -f ../bin/KFC2_exec/core_rim_2
+rm -f ../bin/KFC2_exec/mut_compensation_1
+rm -f ../bin/KFC2_exec/splitpdb
 
 
 gcc -o ../bin/KFC2_exec/core_rim_1 core_rim_1.c
 gcc -o ../bin/KFC2_exec/core_rim_2 core_rim_2.c
-gcc -c -ggdb mut_compensation_1.c
-gcc -o ../bin/KFC2_exec/mut_compensation_1 mut_compensation_1.o
+gcc -o ../bin/KFC2_exec/mut_compensation_1 mut_compensation_1.c
 gcc -o ../bin/KFC2_exec/splitpdb splitpdb.c
 
-echo "Install FADE <------ https://linktofade"
-echo "Install naccess2.1.1 <------ https://linktona"
-echo "Install svm_light <------ https://svm"
+echo ""
+echo ""
+echo "*** DEPENDENCIES ***"
+echo ""
+echo "Install FADE as ./bin/KFC2_exec/FADE"
+echo "https://jcmitchelllab.wordpress.com/2024/07/28/fade-and-padre-molecular-shape-analysis/"
+echo ""
+echo "Install naccess as ./bin/naccess2.1.1/naccess"
+echo "http://www.bioinf.manchester.ac.uk/naccess/nacdownload.html"
+echo ""
+echo "Install svm_classify into ./bin/svm_light"
+echo "https://www.cs.cornell.edu/people/tj/svm_light/"
+echo ""
+echo ""
+echo "*** EXAMPLE ***"
+echo ""
+echo "sh kfc2.sh data/1dfj.pdb A B 1dfj"
